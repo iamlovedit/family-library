3b55ff509d02fd857621467cabc360812c0855cf
Young <iamlovedit@163.com>
2024-12-18 10:10:04 +0800

    updated swagger titles

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,8 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const config = new DocumentBuilder()
-    .setTitle('Cats example')
-    .setDescription('The cats API description')
+    .setTitle('family library')
+    .setDescription('The library API description')
     .setVersion('1.0')
     .addTag('dynamo')
     .build();
